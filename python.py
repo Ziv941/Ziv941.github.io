241f1a51f953d358ae6e4c55799172404410f1a1
Ziv941 <125398249+Ziv941@users.noreply.github.com>
2023-02-14 10:50:49 -0800

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+
+import random
+list_1=['I love you a lot', 'you are an incredible person', 'You are very beautiful ','I cant stop thinking aout you' ,'Do you have any idea how much I adore you?', 'I am always thiking of you','I am very lucky that We are together','You are so special to me',
+        'My favorite sound in the universe is your laugh','You make every day better','Life with you is an adventure','You are all my heart ever talks about','I am counting down the minutes until I can see you again','You are my favorite psycho/person',
+        'I cannot wait to fall asleep beside you','You are so cute','Lets flip a coin: Heads I am yours. Tails you are mine','I heard kissing burns calories. Want to work out with me?','If you keep being so sweet, I will have diabetes very soon','Thank you for always being there for me',
+        'You are such a thoughtful girlfriend']
+picker=random.choice(list_1)
+print(picker)
